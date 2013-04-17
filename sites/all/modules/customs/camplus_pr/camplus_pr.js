750d542dd35f19a18d11f42362a3a33ca7b5b9b9
xiangkeguo <xiangkeguo@gmail.com>
2013-04-17 23:05:59 +0800

    ajax js & callback

--- sites/all/modules/customs/camplus_pr/camplus_pr.js
+++ sites/all/modules/customs/camplus_pr/camplus_pr.js
@@ -1,19 +1,17 @@
  jQuery(function($){ // select change show notice.
  			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
-			  	if($(this).parents('.fieldset-wrapper').find('.warning').length == 0) {
+			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
 			  		// $(this).parents('.fieldset-wrapper').prepend('<div id="op-notice" class="messages warning" style="display: block; ">* Please click \'Filter\' to see your changes, and if the filter button is not here: Please click \'Refine\' to see your changes.</div>');
-			  		console.log('changes');
+			  		
 			  	}
-			  });
+			});
 
 	Drupal.behaviors.pr = {
 		attach: function(context, settings) {
-			  $('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
-			  	if($(this).parents('.fieldset-wrapper').find('.warning').length == 0) {
-			  		// $(this).parents('.fieldset-wrapper').prepend('<div id="op-notice" class="messages warning" style="display: block; ">* Please click \'Filter\' to see your changes, and if the filter button is not here: Please click \'Refine\' to see your changes.</div>');
-			  		console.log('changes');
+			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
+			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
 			  	}
-			  });
+			});
 		}
 	}
 
