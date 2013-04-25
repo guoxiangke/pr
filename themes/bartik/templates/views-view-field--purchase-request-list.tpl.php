271238eea281f9c2aff4f3e51850108389740089
xiangkeguo <xiangkeguo@gmail.com>
2013-04-25 22:30:59 +0800

    finished

--- themes/bartik/templates/views-view-field--purchase-request-list.tpl.php
+++ themes/bartik/templates/views-view-field--purchase-request-list.tpl.php
@@ -21,6 +21,31 @@
  * regardless of any changes in the aliasing that might happen if
  * the view is modified.
  */
-dpm($data);
+if($field->field_alias == 'votingapi_cache_node_value') {
+	$node = node_load($row->nid);
+	$show_flag = TRUE;
+	$flags = flag_get_counts('node', $node->nid, $reset = FALSE);
+	if(isset($flags['paid']) && $flags['paid']) {
+		$show_flag = FALSE;
+	}
+    $widgets = rate_get_active_widgets('node', 'purchase_request');
+    foreach ($widgets as $widget_id => $widget) {
+      //没有权限时，清楚rate
+      $widget->id = $widget_id;
+      //一旦审核通过，所有审批部门角色不可更改即 not show rate.
+      if($widget->name == 'approve') {
+        $approve_status = _rate_pr_check_permissions($node,$widget);
+
+        $widget_name = 'rate_' . $widget->name;
+        if(!$approve_status['permit_flag'] || !$show_flag) {
+          // 根据PR节点的总额确定which审批流程
+          // 在上一个审批流程没有确定前 其他后续
+          $output = '--';
+        }
+        break;
+      }
+    }
+}
+
 ?>
 <?php print $output; ?>
