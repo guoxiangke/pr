082ae69af4fe1ca208bb3ce5bf7101b7f76629b2
xiangkeguo <xiangkeguo@gmail.com>
2013-05-29 12:54:06 +0800

    views and report excel

--- themes/bartik/templates/views-view-field--purchase-request-list--page-pr-my--ops.tpl.php
+++ themes/bartik/templates/views-view-field--purchase-request-list--page-pr-my--ops.tpl.php
@@ -30,7 +30,7 @@
   // 7|Cancelled
  // dpm($row);
  $pr_node = node_load($row->nid);
- $show = array('1','6','7');
+ $show = array('1','5','6','7');
  if(in_array($pr_node->field_pr_status[LANGUAGE_NONE][0]['value'], $show)) {
  }else {
  	print $output;
