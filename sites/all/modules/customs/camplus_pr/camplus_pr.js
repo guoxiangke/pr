4d00059d12c883e35b6f5f40662afc2cf6362280
xiangkeguo <xiangkeguo@gmail.com>
2013-05-31 11:21:48 +0800

    0531

--- sites/all/modules/customs/camplus_pr/camplus_pr.js
+++ sites/all/modules/customs/camplus_pr/camplus_pr.js
@@ -1,24 +1,31 @@
  jQuery(function($){ // select change show notice.
  			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
-			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
+			  	if($(this).parents('tr').find('input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
 			  		// $(this).parents('.fieldset-wrapper').prepend('<div id="op-notice" class="messages warning" style="display: block; ">* Please click \'Filter\' to see your changes, and if the filter button is not here: Please click \'Refine\' to see your changes.</div>');
 			  		
 			  	}
 			});
-
+ 			$('#edit-field-pr-sole-supplier-und').change(function() {
+			     if(this.checked) {
+			         // do something when checked
+			        $('#fgm_node_purchase_request_form_group_referenced-add-more-wrapper').slideUp();
+			     }else {
+			     	$('#fgm_node_purchase_request_form_group_referenced-add-more-wrapper').slideDown();
+			     }
+			 });
 	Drupal.behaviors.pr = {
 		attach: function(context, settings) {
 			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
-			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
+			  	if($(this).parents('tr').find('input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
 			  	}
 			});
 
 			$(".field-name-field-chosen-price input").attr("disabled","disabled");
 			countTotalBI();
 			function countTotalBInoblur() {
-				$('.multiple-field-budget-items-amount input').each(function(){
+				$('#fgm-node-purchase-request-form-group-budgets-values input').each(function(){
 					total = 0;
-					$('.multiple-field-budget-items-amount input').each(function(){
+					$('#fgm-node-purchase-request-form-group-budgets-values input').each(function(){
 						total += parseFloat($(this).val());
 					});
 					total += parseFloat($('#edit-field-payment-from-students input').val());
@@ -39,9 +46,9 @@
 	
 	$(".field-name-field-chosen-price input").attr("disabled","disabled");
 	function countTotalBI() {
-		$('.multiple-field-budget-items-amount input').blur(function(){
+		$('#fgm-node-purchase-request-form-group-budgets-values input').blur(function(){
 			total = 0;
-			$('.multiple-field-budget-items-amount input').each(function(){
+			$('#fgm-node-purchase-request-form-group-budgets-values input').each(function(){
 				total += parseFloat($(this).val());
 			});
 			total += parseFloat($('#edit-field-payment-from-students input').val());
@@ -50,10 +57,10 @@
 	}
 	countTotalBI();
 	function countTotalBInoblur() {
-		$('.multiple-field-budget-items-amount input').each(function(){
+		$('#fgm-node-purchase-request-form-group-budgets-values input').each(function(){
 			total = 0;
-			$('.multiple-field-budget-items-amount input').each(function(){
-				total += parseFloat($(this).val());
+			$('#fgm-node-purchase-request-form-group-budgets-values input').each(function(){
+				total += parseFloat($(this).val()) || 0;
 			});
 			total += parseFloat($('#edit-field-payment-from-students input').val());
 			$('.field-name-field-chosen-price input').val(total);
